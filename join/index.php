2629d4fb25c0d30b1bf9c7d291ddee458d5c3363
TAKUTO ARIMA <ta910arima@gmail.com>
2020-08-22 09:26:41 +0900

    エラーチェック完了

--- join/index.php
+++ join/index.php
@@ -1,3 +1,26 @@
+<?php
+if (!empty($_POST)) {
+
+	if ($_POST['name'] === '') {
+		$error['name'] = 'blank';
+	}
+	if ($_POST['email'] === '') {
+		$error['email'] = 'blank';
+	}
+	if (strlen($_POST['password']) < 4) {
+		$error['password'] = 'length';
+	}
+	if ($_POST['password'] === '') {
+		$error['password'] = 'blank';
+	}
+	if(empty($error)) {
+		header('Location: check.php');
+		exit();
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -20,14 +43,26 @@
 	<dl>
 		<dt>ニックネーム<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="name" size="35" maxlength="255" value="" />
+        	<input type="text" name="name" size="35" maxlength="255"value="<?php print(htmlspecialchars($_POST['name'])); ?>" />
+			<?php if($error['name'] === 'blank') :?>
+			<p class="error">*ニックネームを入力してください</p>
+			<?php endif ; ?>
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+        	<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'])); ?>" />
+			<?php if($error['email'] === 'blank') :?>
+			<p class="error">*メールアドレスを入力してください</p>
+			<?php endif ; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'])); ?>" />
+			<?php if($error['password'] === 'length' || $error['password'] ==='!empty') :?>
+			<p class="error">*パスワードは4文字以上で入力してください</p>
+			<?php endif ; ?>
+			<?php if($error['password'] === 'blank') :?>
+			<p class="error">*パスワードを入力してください</p>
+			<?php endif ; ?>
         </dd>
 		<dt>写真など</dt>
 		<dd>
