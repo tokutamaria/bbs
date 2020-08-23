0e247d6983c2ee571aeac1759467cd55bcc74f6f
TAKUTO ARIMA <ta910arima@gmail.com>
2020-08-23 18:05:28 +0900

    変更

--- index.php
+++ index.php
@@ -19,14 +19,28 @@ $member = $members->fetch();
 
 if (!empty($_POST)) {
   if($_POST['message'] !== '') {
-    $message = $db->prepare('INSERT INTO posts SET member_id=?,message=?,created=NOW()');
+    $message = $db->prepare('INSERT INTO posts SET member_id=?,message=?,reply_message_id=?,created=NOW()');
     $message->execute(array(
       $member['id'],
       $_POST['message'],
-      // $_POST['reply_post_id']
+      $_POST['reply_post_id']
     ));
+    header('Location: index.php');
+    exit();
   }
 }
+
+$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p
+          WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+if (isset($_REQUEST['res'])) {
+   $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p
+    WHERE m.id=p.member_id AND p.id=?');
+    $response->execute(array($_REQUEST['res']));
+
+    $table = $response->fetch();
+    $message ='@' . $table['name'] . '' . $table['message'];
+  }
 ?>
 
 
@@ -52,8 +66,8 @@ if (!empty($_POST)) {
       <dl>
         <dt><?php print(htmlspecialchars($member['name'],ENT_QUOTES));?>さん、メッセージをどうぞ</dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES));?>"/>
         </dd>
       </dl>
       <div>
@@ -63,16 +77,22 @@ if (!empty($_POST)) {
       </div>
     </form>
 
+    <!-- メッセージを表示 -->
+<?php foreach ($posts as $post): ?>
     <div class="msg">
-    <img src="member_picture" width="48" height="48" alt="" />
-    <p><span class="name">（）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="></a>
+    <img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES));?>"
+     width="48" height="48" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES));?>"/>
+    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES));?>
+    <span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES));?>）
+    </span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]</p>
+    <p class="day"><?php print(htmlspecialchars($post['created'], ENT_QUOTES));?><<a href="view.php?id="></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
 [<a href="delete.php?id="
 style="color: #F33;">削除</a>]
     </p>
     </div>
+<?php endforeach; ?>
 
 <ul class="paging">
 <li><a href="index.php?page=">前のページへ</a></li>
